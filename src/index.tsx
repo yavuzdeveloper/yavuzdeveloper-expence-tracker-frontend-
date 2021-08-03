f2037a1ec03c604270a6130a898ad7f5bc574307
Yavuz Tokus <yavuztokus2010@gmail.com>
2021-08-03 22:06:26 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from "./store";
+
+
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>, 
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
